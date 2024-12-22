5b5bcfac27fadbc3e6d412ed2460e2bed22d51a3
sergei_vokhminov <rreaper@list.ru>
2024-12-22 20:45:49 +0300

    исправлены ошибки в приложении users

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="catalog:home"), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="mailing:home"), name="logout"),
     path("register/", UserRegisterView.as_view(), name="register"),
 ]
