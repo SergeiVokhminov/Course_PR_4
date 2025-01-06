ce974a904853d71bc6d7f12d36ee6c65d54501be
sergei_vokhminov <rreaper@list.ru>
2025-01-06 10:43:25 +0300

    обновлены настройки проекта

--- config/settings.py
+++ config/settings.py
@@ -107,10 +107,10 @@ LOGIN_REDIRECT_URL = "mailing:home"
 LOGOUT_REDIRECT_URL = "mailing:home"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 465
-# EMAIL_HOST = "smtp.mail.ru"
-# EMAIL_PORT = 2525
+# EMAIL_HOST = "smtp.yandex.ru"
+# EMAIL_PORT = 465
+EMAIL_HOST = "smtp.mail.ru"
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
